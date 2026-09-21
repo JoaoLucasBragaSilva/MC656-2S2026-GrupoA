7fb7b14e703f5394247262509c698976f7abc8c5
Gustavo Borba <gustavo.borba2001@gmail.com>
2026-09-21 10:27:19 -0300

    feature: implementação de testes para a renderização da tela

--- evoting_project/accounts/tests.py
+++ evoting_project/accounts/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomeViewTest(TestCase):
+  def test_home_page(self):
+    response = self.client.get(reverse('home'))  # ajuste o nome da url
+
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'accounts/home.html')
+
+    # Textos principais
+    self.assertContains(response, "Escolha seu tipo de cadastro")
+    self.assertContains(response, "Eleitor")
+    self.assertContains(response, "Partido Político")
+    self.assertContains(response, "Pessoa Física")
+    self.assertContains(response, "Pessoa Jurídica")
+
+    # Footer de login
+    self.assertContains(response, "Já tem uma conta?")
+    self.assertContains(response, "Fazer login")
+
+    # Estrutura dos cards
+    self.assertContains(response, 'data-type="voter"')
+    self.assertContains(response, 'data-type="party"')
+    self.assertContains(response, 'class="card-stripe voter-stripe"')
+    self.assertContains(response, 'class="card-stripe party-stripe"')
+
+    # CSS carregado
+    self.assertContains(response, 'home.css')
--- evoting_project/evoting/settings.py
+++ evoting_project/evoting/settings.py
@@ -12,24 +12,18 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# --- Security ---
+# NOTE: This key is for local development only. Generate a new one before
+# deploying anywhere public (e.g. with django.core.management.utils.get_random_secret_key()).
+SECRET_KEY = 'dev-only-secret-key-change-me'
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jk&nmg_&rr-8+eec=u&-0e-i%12iac7atl422tk$udtv%@nivm'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
-
-# Application definition
-
+# --- Applications ---
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +53,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -68,10 +64,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'evoting.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/6.1/ref/settings/#databases
-
+# --- Database (simple local SQLite file) ---
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -79,49 +72,18 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/6.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
 
-
-# Email
-# https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
-
-MAILERS = {
-    'default': {
-        'BACKEND': 'django.core.mail.backends.console.EmailBackend',
-    },
-}
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
